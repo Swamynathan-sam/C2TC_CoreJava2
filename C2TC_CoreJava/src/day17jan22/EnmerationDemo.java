a4f309b3b3141d2f37a55e0a5ac3b4af12af424d
Swamynathan-sam <swamynathansam7@gmail.com>
2022-01-20 20:12:28 +0530

    First Commit

--- C2TC_CoreJava/src/day17jan22/EnmerationDemo.java
+++ C2TC_CoreJava/src/day17jan22/EnmerationDemo.java
@@ -1,7 +1,12 @@
+/**
+ * @author SWAMYNATHAN G.H
+ */
+//Enumerator introduced in 1.0Version of Java
 package day17jan22;
+
 import java.util.Vector;
+
 import java.util.Enumeration;
-import java.util.List;
 
 public class EnmerationDemo {
 	public static void main(String[] args) {
@@ -12,13 +17,25 @@ public class EnmerationDemo {
 		vector.add(5);
 		vector.add(2);
 
+	/**
+	 * we can create enumeration object by using elements() method. 
+	 * Enumeration is used in legacy classes(It's not a Universal cursor)
+	 *  objects to get Objects one by one.
+	 */
 		Enumeration<Integer> elements = vector.elements();
-		while (elements.hasMoreElements()) {
+		/**
+		 * 
+		 */
+		while (elements.hasMoreElements()) { 
 			Integer n = elements.nextElement();
 			System.out.println(n);
-			
+	/**
+	 * Enumeration can perform read operation,
+	 * we can't perform write Operation.
+	 */
+
 		}
-        System.out.println("------------------------------------------");
+		System.out.println("------------------------------------------");
 		System.out.println(vector);
 	}
 
